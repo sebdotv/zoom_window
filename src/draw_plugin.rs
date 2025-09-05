2b6b682f7e7f94d1a8b5a22b58e4b07521eac360
sebdotv <sebdotv@gmail.com>
2025-09-05 17:47:22 +0200

    show Haversine distance in NM

--- src/draw_plugin.rs
+++ src/draw_plugin.rs
@@ -1,6 +1,9 @@
-use egui::{Color32, Response, Ui};
-use walkers::Projector;
+use egui::{Color32, Response, Stroke, Ui};
+use geo::{Distance, Haversine};
+use uom::si::f64::Length;
+use uom::si::length::{meter, nautical_mile};
 use walkers::{MapMemory, Position};
+use walkers::{Projector, lat_lon};
 
 #[derive(Copy, Clone)]
 enum State {
@@ -63,29 +66,45 @@ impl walkers::Plugin for &mut DrawPlugin {
         match self.state {
             State::Idle => {}
             State::StartPoint { start, hover } => {
-                // draw line from start to hover
                 if let Some(hover) = hover {
-                    let start_pos = projector.project(start).to_pos2();
-                    let end_pos = projector.project(hover).to_pos2();
-                    let line = egui::Shape::line_segment(
-                        [start_pos, end_pos],
-                        egui::Stroke::new(1.0, Color32::RED),
-                    );
-                    ui.painter().add(line);
+                    draw_segment(ui, start, hover, projector, (1.0, Color32::RED));
                 }
-
-                // draw start point
                 ui.painter()
                     .circle_filled(projector.project(start).to_pos2(), 5.0, Color32::GREEN);
             }
             State::Line { start, end } => {
-                let start_pos = projector.project(start).to_pos2();
-                let end_pos = projector.project(end).to_pos2();
+                draw_segment(ui, start, end, projector, (3.0, Color32::RED));
                 ui.painter()
-                    .line_segment([start_pos, end_pos], (3.0, Color32::RED));
-                ui.painter().circle_filled(start_pos, 5.0, Color32::GREEN);
-                ui.painter().circle_filled(end_pos, 5.0, Color32::BLUE);
+                    .circle_filled(projector.project(start).to_pos2(), 5.0, Color32::GREEN);
+                ui.painter()
+                    .circle_filled(projector.project(end).to_pos2(), 5.0, Color32::BLUE);
             }
         }
     }
+}
+
+fn draw_segment(
+    ui: &mut Ui,
+    start: Position,
+    end: Position,
+    projector: &Projector,
+    stroke: impl Into<Stroke>,
+) {
+    // draw line from start to end
+    let start_pos = projector.project(start).to_pos2();
+    let end_pos = projector.project(end).to_pos2();
+    ui.painter().line_segment([start_pos, end_pos], stroke);
+
+    // draw distance text
+    let mid_point = lat_lon((start.y() + end.y()) / 2.0, (start.x() + end.x()) / 2.0);
+    let mid_pos = projector.project(mid_point).to_pos2();
+    let distance = Length::new::<meter>(Haversine.distance(start, end));
+    let text = format!("{:.1} NM", distance.get::<nautical_mile>());
+    ui.painter().text(
+        mid_pos,
+        egui::Align2::CENTER_CENTER,
+        text,
+        egui::FontId::default(),
+        Color32::BLACK,
+    );
 }
